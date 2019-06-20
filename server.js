cc149df1fed1d6aba218e68f931cd46c84af7ba6
Lauren Lee <Laurenelee88@gmail.com>
2019-06-20 10:16:41 -0700

    change port

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 
-const port = 5000;
+// const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server started on port ${port}`));
 
