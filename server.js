a18a70abb6d43262496d983eb17c8485ea692e06
Lauren Lee <Laurenelee88@gmail.com>
2019-06-21 12:25:55 -0700

    typo fix

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.use(bodyParser.json());
 
 // heroku handling
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('clien/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(_dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   })
 }
 
